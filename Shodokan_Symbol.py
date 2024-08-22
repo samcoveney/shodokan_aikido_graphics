1264b5fffe8f9f459a1e0afda37bd841b16b9f78
Sam Coveney <s.coveney@leeds.ac.uk>
2024-08-22 09:19:44 +0100

    flower done

--- Shodokan_Symbol.py
+++ Shodokan_Symbol.py
@@ -13,8 +13,6 @@ def star(x,y,radius):
     ctx.fill()
 
 
-# Open image to an ARGB32 ImageSurface
-filename = 'Shodokan_Symbol.png'
 
 # set the canvas
 # ==============
@@ -33,16 +31,48 @@ pdf = cairo.PDFSurface( pdfname,
 ctx = cairo.Context(pdf)
 ctx.scale(point_to_millimeter, point_to_millimeter)
 
+# Open image to an ARGB32 ImageSurface
+if True:
+    filename = 'Shodokan_Symbol.png'
+    ctx.select_font_face("Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+
+    img = cairo.ImageSurface.create_from_png(filename)
+    ctx.save()
+    ctx.scale(0.25, 0.25)
+    margin=10
+    ctx.set_source_surface(img, margin, margin+15)
+    ctx.paint()
+    ctx.restore()
+    #ctx.set_source_surface(img, 100, 100)
+    #ctx.paint()
+
+
+# ---- PDF drawing ----
+
+
 ox, oy = 150, 150
 
 
 sang = 2*math.pi/8.0
 
-radius = 100 
+radius = 140 
 
-A, B, C = 0.93, 1.01, 1.01 #0.9750
+
+if 0:
+    RED = [256.0/256., 9.0/256., 0.0/256.]
+    BLUE = [16/255.,63/255.,188/255.]
+    GRAY = [135/255.,141/255.,145/255.]
+else: # for testing if my shape lines up with old image
+    RED = [0, 1, 1]
+    BLUE = [0, 1, 1]
+    GRAY = [0, 1, 1]
+
+
+
+A, B, C = 0.96, 1.07, 0.95 #0.9750
+DIV=128.
 ctx.set_source_rgb(0, 0, 1)
-radius = radius * A# NOTE: here, for the outer flower, we have a multiply A
+radius = radius * 0.91 # NOTE: here, for the outer flower, we have a multiply A
 ax, ay = ox, oy + radius 
 ctx.move_to(ax, ay) # starting point
 for arc in range(1, 9):
@@ -56,11 +86,11 @@ for arc in range(1, 9):
 
     # TODO: move the spline control points inwards
 
-    bx = ox + radius * np.sin(ang - sang * 1/24) * B 
-    by = oy + radius * np.cos(ang - sang * 1/24) * B
+    bx = ox + radius * np.sin(ang - sang * 1/DIV) * B 
+    by = oy + radius * np.cos(ang - sang * 1/DIV) * B
 
-    cx = ox + radius * np.sin(ang - sang * 2/24.) * C
-    cy = oy + radius * np.cos(ang - sang * 2/24.) * C
+    cx = ox + radius * np.sin(ang - sang * 2/DIV) * C
+    cy = oy + radius * np.cos(ang - sang * 2/DIV) * C
 
     ctx.curve_to(bx, by, cx, cy, dx, dy)
     #ctx.set_source_rgb(0, 0, 1)
@@ -75,11 +105,11 @@ for arc in range(1, 9):
 
     # TODO: move the spline control points inwards
 
-    bx = ox + radius * np.sin(ang - 0.5*sang + sang * 2/24) * C 
-    by = oy + radius * np.cos(ang - 0.5*sang + sang * 2/24) * C
+    bx = ox + radius * np.sin(ang - 0.5*sang + sang * 2/DIV) * C 
+    by = oy + radius * np.cos(ang - 0.5*sang + sang * 2/DIV) * C
 
-    cx = ox + radius * np.sin(ang - 0.5*sang + sang * 1/24.) * B
-    cy = oy + radius * np.cos(ang - 0.5*sang + sang * 1/24.) * B
+    cx = ox + radius * np.sin(ang - 0.5*sang + sang * 1/DIV) * B
+    cy = oy + radius * np.cos(ang - 0.5*sang + sang * 1/DIV) * B
 
     ctx.curve_to(bx, by, cx, cy, dx, dy)
     #ctx.set_source_rgb(0, 1, 0)
@@ -88,65 +118,69 @@ for arc in range(1, 9):
 
 
 ctx.set_line_width(1.5) 
-ctx.set_source_rgb(135/255.,141/255.,145/255.)
+ctx.set_source_rgb(*GRAY)
 ctx.stroke()
 
-radius = radius / A # NOTE: here, for the outer flower, we have a multiply A
-
-for FILL in [True, False]:
-    A, B, C = 0.50, 0.62, 0.52
-    ctx.set_source_rgb(0, 0, 1)
-    ax, ay = ox, oy + radius
-    ctx.move_to(ax, ay) # starting point
-    for arc in range(1, 9):
-
-        # bezier to half-way
-        ang = arc * sang - sang/2.0
-        # NOTE: factor of 0.5 to determine finishing posiiton
-        dx = ox + radius*A * np.sin(ang)
-        dy = oy + radius*A * np.cos(ang)
-        print(dx, dy)
-
-        # TODO: move the spline control points inwards
-
-        bx = ox + radius * np.sin(ang - sang * 4/8) * B 
-        by = oy + radius * np.cos(ang - sang * 4/8) * B
-
-        cx = ox + radius * np.sin(ang - sang * 3/8.) * C
-        cy = oy + radius * np.cos(ang - sang * 3/8.) * C
-
-        ctx.curve_to(bx, by, cx, cy, dx, dy)
-        #ctx.set_source_rgb(0, 0, 1)
-        #ctx.stroke()
-        #ctx.move_to(dx, dy) # starting point
+radius = radius / 0.91 # NOTE: here, for the outer flower, we have a multiply A
 
 
-        # rest of the way
-        ang = arc * sang
-        dx = ox + radius * np.sin(ang)
-        dy = oy + radius * np.cos(ang)
+# === blue star ===
 
-        # TODO: move the spline control points inwards
+if False:
 
-        bx = ox + radius * np.sin(ang - 0.5*sang + sang * 3/8) * C 
-        by = oy + radius * np.cos(ang - 0.5*sang + sang * 3/8) * C
+    for FILL in [True, False]:
+        A, B, C = 0.50, 0.62, 0.52
+        ax, ay = ox, oy + radius
+        ctx.move_to(ax, ay) # starting point
+        for arc in range(1, 9):
 
-        cx = ox + radius * np.sin(ang - 0.5*sang + sang * 4/8.) * B
-        cy = oy + radius * np.cos(ang - 0.5*sang + sang * 4/8.) * B
+            # bezier to half-way
+            ang = arc * sang - sang/2.0
+            # NOTE: factor of 0.5 to determine finishing posiiton
+            dx = ox + radius*A * np.sin(ang)
+            dy = oy + radius*A * np.cos(ang)
+            print(dx, dy)
 
-        ctx.curve_to(bx, by, cx, cy, dx, dy)
-        #ctx.set_source_rgb(0, 1, 0)
-        #ctx.stroke()
-        #ctx.move_to(dx, dy) # starting point
+            # TODO: move the spline control points inwards
+
+            bx = ox + radius * np.sin(ang - sang * 4/8) * B 
+            by = oy + radius * np.cos(ang - sang * 4/8) * B
+
+            cx = ox + radius * np.sin(ang - sang * 3/8.) * C
+            cy = oy + radius * np.cos(ang - sang * 3/8.) * C
+
+            ctx.curve_to(bx, by, cx, cy, dx, dy)
+            #ctx.set_source_rgb(0, 0, 1)
+            #ctx.stroke()
+            #ctx.move_to(dx, dy) # starting point
 
 
-    if FILL:
-        ctx.set_source_rgb(16/255.,63/255.,188/255.)
-        ctx.fill()
-    else:
-        ctx.set_line_width(1.5) 
-        ctx.set_source_rgb(135/255.,141/255.,145/255.)
-        ctx.stroke()
+            # rest of the way
+            ang = arc * sang
+            dx = ox + radius * np.sin(ang)
+            dy = oy + radius * np.cos(ang)
+
+            # TODO: move the spline control points inwards
+
+            bx = ox + radius * np.sin(ang - 0.5*sang + sang * 3/8) * C 
+            by = oy + radius * np.cos(ang - 0.5*sang + sang * 3/8) * C
+
+            cx = ox + radius * np.sin(ang - 0.5*sang + sang * 4/8.) * B
+            cy = oy + radius * np.cos(ang - 0.5*sang + sang * 4/8.) * B
+
+            ctx.curve_to(bx, by, cx, cy, dx, dy)
+            #ctx.set_source_rgb(0, 1, 0)
+            #ctx.stroke()
+            #ctx.move_to(dx, dy) # starting point
+
+
+        if FILL:
+            ctx.set_source_rgb(*BLUE)
+            ctx.fill()
+        else:
+            ctx.set_line_width(1.5) 
+            ctx.set_source_rgb(*GRAY)
+            ctx.stroke()
 
 if False:
     ctx.set_source_rgb(0, 0.5, 0.5)
@@ -173,27 +207,17 @@ if False:
     #ctx.stroke()
     ctx.fill()
 
-ctx.set_source_rgb(135/255.,141/255.,145/255.)
-circle(ox, oy, 15)
-ctx.set_source_rgb(256.0/256., 9.0/256., 0.0/256.)
-circle(ox, oy, 13.5)
+# === red circle ===
+if False:
+    ctx.set_source_rgb(*GRAY)
+    circle(ox, oy, 15)
+    ctx.set_source_rgb(*RED)
+    circle(ox, oy, 13.5)
 
 
 
 #ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
 
-if False:
-    ctx.select_font_face("Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-
-    img = cairo.ImageSurface.create_from_png(filename)
-    ctx.save()
-    ctx.scale(0.25, 0.25)
-    margin=10
-    ctx.set_source_surface(img, margin, margin)
-    ctx.paint()
-    ctx.restore()
-    #ctx.set_source_surface(img, 100, 100)
-    #ctx.paint()
 
 pdf.show_page()
 
