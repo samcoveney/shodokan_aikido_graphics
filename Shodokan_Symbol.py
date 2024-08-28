101a7ee985e871f6ae4546955e056eb803fcb1eb
Sam Coveney <s.coveney@leeds.ac.uk>
2024-08-28 10:59:41 +0100

    thicker arms 2

--- Shodokan_Symbol.py
+++ Shodokan_Symbol.py
@@ -136,7 +136,7 @@ def main(TEST, FEATURE, OUTLINE, PDFNAME):
     if not TEST or FEATURE == "star":
 
         for FILL in [True, False]:
-            A, B, C = 0.44, 0.6, 0.46
+            A, B, C = 0.44, 0.6, 0.47
             DIV = 10.0
             BUF = 0.00
             #BUF = -0.002
@@ -159,8 +159,9 @@ def main(TEST, FEATURE, OUTLINE, PDFNAME):
                     bx = ox + radius * np.sin(ang - sang * 4.2/DIV) * B 
                     by = oy + radius * np.cos(ang - sang * 4.2/DIV) * B
 
-                    cx = ox + radius * np.sin(ang - sang * 4/DIV) * C
-                    cy = oy + radius * np.cos(ang - sang * 4/DIV) * C
+                    a2 = 4.5
+                    cx = ox + radius * np.sin(ang - sang * a2/DIV) * C
+                    cy = oy + radius * np.cos(ang - sang * a2/DIV) * C
 
                     ctx.curve_to(bx, by, cx, cy, dx, dy)
                     #ctx.set_source_rgb(0, 0, 1)
@@ -180,8 +181,9 @@ def main(TEST, FEATURE, OUTLINE, PDFNAME):
 
                     # TODO: move the spline control points inwards
 
-                    bx = ox + radius * np.sin(ang - 0.5*sang + sang * 4/DIV) * C 
-                    by = oy + radius * np.cos(ang - 0.5*sang + sang * 4/DIV) * C
+                    a2 = 4.5
+                    bx = ox + radius * np.sin(ang - 0.5*sang + sang * a2/DIV) * C 
+                    by = oy + radius * np.cos(ang - 0.5*sang + sang * a2/DIV) * C
 
                     cx = ox + radius * np.sin(ang - 0.5*sang + sang * 4.2/DIV) * B
                     cy = oy + radius * np.cos(ang - 0.5*sang + sang * 4.2/DIV) * B
